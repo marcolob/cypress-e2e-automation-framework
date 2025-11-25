9db7fbbd4c01696a42196579c2a70232942b4570
Lob-coder <74981656+Lob-coder@users.noreply.github.com>
2025-11-25 14:07:33 +0100

    Add GitHub Actions workflow and refactor Cypress tests

--- cypress/e2e/hello.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Hello World Test', () => {
-    it('Visita Google e controlla il titolo', () => {
-      cy.visit('https://www.google.com')
-      cy.title().should('include', 'Google')
-    })
-  })
-  
--- cypress/e2e/link.cy.js
+++
@@ -1,8 +0,0 @@
-// 2. Test link
-describe('Test Link', () => {
-    it('Clicca sul link "type" e verifica URL', () => {
-      cy.visit('https://example.cypress.io')
-      cy.contains('type').click()
-      cy.url().should('include', '/commands/actions')
-    })
-  })
